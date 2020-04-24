d146ec691f77b900f7b552b0935aefe9f0b6c56d
mojikimino <coffeeoriginal0@gmail.com>
2020-04-24 19:25:16 +0900

    update 0.0.5

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,7 +1,12 @@
 from mkn import sample
 
 
-def test_say():
+def test_work_well():
     expected = "ok"
     result = sample.work_well()
+    assert result == expected
+
+def test_say_hello():
+    expected = "hello"
+    result = sample.say_hello()
     assert result == expected
