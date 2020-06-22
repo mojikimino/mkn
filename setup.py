551aca97058fb1802f4785b9f357074468063a36
mojikimino <coffeeoriginal0@gmail.com>
2020-06-23 01:42:12 +0900

    fix setup.py

--- setup.py
+++ setup.py
@@ -13,5 +13,5 @@ setup(
     download_url="https://github.com/mojikimino/mkn/archive/{}.tar.gz".format(__version__),
     packages=["mkn"],
     include_package_data=True,
-    install_requires=["pytest"]
+    install_requires=["pytest", "tqdm"]
 )   
